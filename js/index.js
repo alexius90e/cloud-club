bb18880feb9a5dffaefcb609dc141c5764137267
Aliaksei Karzhou <alexius90e@gmail.com>
2024-05-12 13:44:11 +0300

    fix: fixed mobile first screen height

--- js/index.js
+++ js/index.js
@@ -56,3 +56,11 @@ if (contactForm) {
     if (successModal) successModal.classList.add('active');
   });
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
